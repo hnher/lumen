e7b6a6ae7bfa1562acf1dc520bad94483d6b57db
Retr0 <dyy@dyy.name>
2020-06-18 17:08:10 +0800

    更新数据库

--- config/database.php
+++ config/database.php
@@ -32,27 +32,28 @@ return [
     */
 
     'connections' => [
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => env('DB_PREFIX', ''),
-        ],
-
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 3306),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => env('DB_PREFIX', ''),
-            'strict' => env('DB_STRICT_MODE', true),
-            'engine' => env('DB_ENGINE', null),
-            'timezone' => env('DB_TIMEZONE', '+00:00'),
+            'read' => [
+                'host' => [
+                    env('DB_MASTER_READ_HOST')
+                ]
+            ],
+            'write' => [
+                'host' => [
+                    env('DB_MASTER_WRITE_HOST')
+                ]
+            ],
+            'database' => env('DB_MASTER_DATABASE', 'forge'),
+            'username' => env('DB_MASTER_USERNAME', 'forge'),
+            'password' => env('DB_MASTER_PASSWORD', ''),
+            'unix_socket' => env('DB_MASTER_SOCKET', ''),
+            'charset' => env('DB_MASTER_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_MASTER_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => env('DB_MASTER_PREFIX', ''),
+            'strict' => env('DB_MASTER_STRICT_MODE', true),
+            'engine' => env('DB_MASTER_ENGINE', null),
+            'timezone' => env('DB_MASTER_TIMEZONE', '+00:00'),
         ]
 
     ],
