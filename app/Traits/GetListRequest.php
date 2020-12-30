7e6d2a45338fb929c4c0aaf23109052852616ee8
hnher <dyy@dyy.name>
2020-12-30 17:24:51 +0800

    新增默认列表请求验证

---
+++ app/Traits/GetListRequest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * GetListRequest.php
+ * Create By PhpStorm
+ * User hnher
+ * Date 2020/12/30
+ * Time 5:21 下午
+ */
+
+namespace App\Traits;
+
+/**
+ * 获取列表基类
+ * Trait GetListRequest
+ * @package App\Traits
+ */
+trait GetListRequest
+{
+    /**
+     * 验证规则
+     * @return string[]
+     */
+    public function rule(): array
+    {
+        return [
+            'page' => 'integer',
+            'limit' => 'integer',
+        ];
+    }
+
+    /**
+     * 异常消息
+     * @return string[]
+     */
+    public function messages(): array
+    {
+        return [
+            'page.integer' => '参数:page 必须是整数',
+            'limit.integer' => '参数:limit 必须是整数'
+        ];
+    }
+}
