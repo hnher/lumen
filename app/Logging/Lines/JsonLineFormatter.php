87d8e3874e8c33f0be067eaccbb7a86f8c88f72b
hnher <dyy@dyy.name>
2021-03-23 11:39:45 +0800

    新增日志

--- app/Logging/Lines/JsonLineFormatter.php
+++ app/Logging/Lines/JsonLineFormatter.php
@@ -28,6 +28,7 @@ class JsonLineFormatter extends LineFormatter
         $ua = $_SERVER['HTTP_USER_AGENT'] ?? '';
         $url = $protocol . $host . $uri;
         $cookies = Json::encode($_COOKIE ?? "");
+        $authorization = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
         if (strpos(php_sapi_name(), 'cli') !== false) {
             $url = 'artisan';
             $referer = 'artisan';
@@ -36,7 +37,7 @@ class JsonLineFormatter extends LineFormatter
 
         $datetime = date('Y-m-d H:i:s', time());
 
-        $output = '{"app": "%app%", "datetime": "' . $datetime . '", "timestamp": "%datetime%", "url": "' . $url . '", "UA": "' . $ua . '", "referer": "' . $referer . '", "uuid": %uuid%, "domain": "%domain%", "channel": "%channel%", "level": "%level_name%", "message": "%message%", "context": [%context%], "extra": %extra%, "cookies": ' . $cookies . '}' . "\n";
+        $output = '{"app": "%app%", "authorization": "'. $authorization .'", "datetime": "' . $datetime . '", "timestamp": "%datetime%", "url": "' . $url . '", "UA": "' . $ua . '", "referer": "' . $referer . '", "uuid": %uuid%, "domain": "%domain%", "channel": "%channel%", "level": "%level_name%", "message": "%message%", "context": [%context%], "extra": %extra%, "cookies": ' . $cookies . '}' . "\n";
         $vars = (new NormalizerFormatter())->format($record);
         $vars['app'] = config('app.name');
         $vars['channel'] = $vars['context']['channel'] ?? 'local';
