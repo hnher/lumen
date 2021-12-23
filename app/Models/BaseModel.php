6fa69e0e641f4e2b73ae5ac66d1b204b113a2f28
hnher <dyy@dyy.name>
2021-12-23 11:00:46 +0800

    优化model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Closure;
 use DateTimeInterface;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * App\Models\BaseModel
@@ -32,7 +33,35 @@ use DateTimeInterface;
  */
 class BaseModel extends Model
 {
+    use SoftDeletes;
+
     protected $connection = 'master';
+
+    public const CREATED_AT = 'createdTime';
+
+    public const UPDATED_AT = 'updatedTime';
+
+    public const DELETED_AT = 'deletedTime';
+
+    protected $dateFormat = 'U';
+
+    public $guarded = ['id', 'createdTime', 'updatedTime', 'deletedTime'];
+
+    public $hidden = ['id', 'appId', 'platformId', 'userId', 'sellerId', 'deletedTime', 'unitId', 'activityId', 'planId', 'couponId', 'partnerId', 'paymentId', 'orderId', 'billId'];
+
+    protected $casts = [
+        'createdTime' => 'datetime:Y-m-d H:i:s',
+        'updatedTime' => 'datetime:Y-m-d H:i:s',
+        'deletedTime' => 'datetime:Y-m-d H:i:s',
+        'startTime' => 'datetime:Y-m-d H:i:s',
+        'endTime' => 'datetime:Y-m-d H:i:s',
+        'receiveTime' => 'datetime:Y-m-d H:i:s',
+        'expireTime' => 'datetime:Y-m-d H:i:s',
+        'useTime' => 'datetime:Y-m-d H:i:s',
+        'verifyTime' => 'datetime:Y-m-d H:i:s',
+        'paidTime' => 'datetime:Y-m-d H:i:s',
+        'refundTime' => 'datetime:Y-m-d H:i:s',
+    ];
 
     /**
      * 保持时间与时区一致
@@ -42,5 +71,23 @@ class BaseModel extends Model
     protected function serializeDate(DateTimeInterface $date)
     {
         return $date->format($this->dateFormat ?: 'Y-m-d H:i:s');
+    }
+
+    public static function uuid()
+    {
+        return base_convert(rand(1000, 9999) . date('YmdHis') . rand(1000, 9999), 10, 36);
+    }
+    /**
+     * 自动写入应用ID 和 平台ID
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating( function ($model) {
+            $attributes = $model->getAttributes();
+            $model->appId = empty($attributes['appId']) ? User::get('appId') : $attributes['appId'];
+            $model->platformId = empty($attributes['platformId']) ? User::get('platform') : $attributes['platformId'];
+            $model->uuid = empty($attributes['uuid']) ? self::uuid() : $attributes['uuid'];
+        });
     }
 }
